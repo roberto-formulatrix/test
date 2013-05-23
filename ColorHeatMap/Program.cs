b64d928416e96e573b1034845c28c6f5ae00e43e
Roberto Dorich <roberto@formulatrix.com>
2013-05-23 15:44:22 -0400

    change

--- ColorHeatMap/Program.cs
+++ ColorHeatMap/Program.cs
@@ -16,6 +16,7 @@ namespace ColorHeatMap
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+// test
         }
     }
 }
